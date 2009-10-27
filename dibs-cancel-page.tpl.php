b0e02c7697864390f36103faad9686e12e0c8718
Mikkel Høgh <mikkel@hoegh.org>
2009-10-27 14:57:13 +0000

    Less aggressive cancellation message.

--- dibs-cancel-page.tpl.php
+++ dibs-cancel-page.tpl.php
@@ -17,6 +17,6 @@
  */
 ?>
 <div id="dibs-cancel-page-<?php print $transaction['api_module']; ?>-<?php print $transaction['api_delta']; ?>" class="dibs-cancel-page clear-block">
-  <p><?php print t('You decided to click cancel in the payment form. Please click the button below to return to the payment again.'); ?></p>
+  <p><?php print t('The payment form was cancelled. Please click the button below to return to the payment again.'); ?></p>
   <?php print $form; ?>
 </div>
